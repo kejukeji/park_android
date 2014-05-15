549f5c880957b06fe5485ff6b8f3a846c52e3c59
zhoujun <zhoujun@192.168.1.109>
2014-05-15 17:00:15 +0800

    1.语音界面报用户语音输入

--- src/com/keju/park/ui/searchparking/VoiceSearchActivity.java
+++ src/com/keju/park/ui/searchparking/VoiceSearchActivity.java
@@ -2,6 +2,7 @@ package com.keju.park.ui.searchparking;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -14,9 +15,12 @@ import com.iflytek.cloud.speech.SpeechConstant;
 import com.iflytek.cloud.speech.SpeechError;
 import com.iflytek.cloud.speech.SpeechListener;
 import com.iflytek.cloud.speech.SpeechRecognizer;
+import com.iflytek.cloud.speech.SpeechSynthesizer;
 import com.iflytek.cloud.speech.SpeechUser;
+import com.iflytek.cloud.speech.SynthesizerListener;
 import com.iflytek.cloud.ui.RecognizerDialog;
 import com.iflytek.cloud.ui.RecognizerDialogListener;
+import com.keju.park.CommonApplication;
 import com.keju.park.R;
 import com.keju.park.ui.base.BaseActivity;
 import com.keju.park.util.JsonParser;
@@ -38,14 +42,17 @@ public class VoiceSearchActivity extends BaseActivity implements OnClickListener
 	private RecognizerDialog iatDialog;
 	// 缓存，保存当前的引擎参数到下一次启动应用程序使用.
 	private SharedPreferences mSharedPreferences;
-	
+	//合成对象.
+	private SpeechSynthesizer mSpeechSynthesizer;
 	private String voiceStr;
+	
+	private CommonApplication app;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.voice_search);
-
+		app = (CommonApplication) getApplication();
 		initBar();
 
 		findView();
@@ -89,9 +96,39 @@ public class VoiceSearchActivity extends BaseActivity implements OnClickListener
 		iatRecognizer = SpeechRecognizer.createRecognizer(this);
 		// 初始化听写Dialog,如果只使用有UI听写功能,无需创建SpeechRecognizer
 		iatDialog = new RecognizerDialog(this);
-
+		// 初始化合成对象.
+		mSpeechSynthesizer = SpeechSynthesizer.createSynthesizer(this);
+		
 	}
-
+	/**
+	 * 使用SpeechSynthesizer合成语音
+	 * 
+	 * @param
+	 */
+	private void synthetizeInSilence(String source) {
+		if (null == mSpeechSynthesizer) {
+			// 创建合成对象.
+			mSpeechSynthesizer = SpeechSynthesizer.createSynthesizer(this);
+		}
+		// 设置合成发音人.
+		String role = "xiaoyan";
+		// 设置发音人
+		mSpeechSynthesizer.setParameter(SpeechConstant.VOICE_NAME, role);
+		// 获取语速
+		int speed = 20;
+		// 设置语速
+		mSpeechSynthesizer.setParameter(SpeechConstant.SPEED, "" + speed);
+		// 获取音量.
+		int volume = 100;
+		// 设置音量
+		mSpeechSynthesizer.setParameter(SpeechConstant.VOLUME, "" + volume);
+		// 获取语调
+		int pitch = 49;
+		// 设置语调
+		mSpeechSynthesizer.setParameter(SpeechConstant.PITCH, "" + pitch);
+		
+		mSpeechSynthesizer.startSpeaking(source, synthesizerListener);
+	}
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
@@ -101,24 +138,7 @@ public class VoiceSearchActivity extends BaseActivity implements OnClickListener
 		case R.id.btnVoiceSearch:
 			rlErroeOrRight.setVisibility(View.VISIBLE);
 			tvSpeak.setText("你是否说的是");
-			boolean isShowDialog = mSharedPreferences.getBoolean(getString(R.string.preference_key_iat_show), true);
-			if (isShowDialog) {
-				// 显示语音听写Dialog.
-				showIatDialog();
-			} else {
-				if (null == iatRecognizer) {
-					iatRecognizer = SpeechRecognizer.createRecognizer(this);
-				}
-				if (iatRecognizer.isListening()) {
-					iatRecognizer.stopListening();
-					showShortToast("停止录音");
-					((Button) findViewById(android.R.id.button1)).setEnabled(false);
-				} else {
-					// 不显示Dialog.
-					showIatInvisble();
-					((Button) findViewById(android.R.id.button1)).setText("停止");
-				}
-			}
+			showIatDialog();
 			break;
 		case R.id.btnError:
 		 tvSpeak.setText(R.string.speak);
@@ -148,7 +168,7 @@ public class VoiceSearchActivity extends BaseActivity implements OnClickListener
 			iatDialog = new RecognizerDialog(this);
 		}
 		// 获取引擎参数
-		String engine = "poi";
+		String engine = "poi_engine";
 
 		// 清空Grammar_ID，防止识别后进行听写时Grammar_ID的干扰
 		iatDialog.setParameter(SpeechConstant.CLOUD_GRAMMAR, null);
@@ -163,14 +183,15 @@ public class VoiceSearchActivity extends BaseActivity implements OnClickListener
 			iatDialog.setParameter(SpeechConstant.SAMPLE_RATE, "16000");
 		}
 
-		// 当DOMAIN设置为POI搜索时,获取area参数
-
-		String province = mSharedPreferences.getString(getString(R.string.preference_key_poi_province),
-				getString(R.string.preference_default_poi_province));
-		String city = mSharedPreferences.getString(getString(R.string.preference_key_poi_city),
-				getString(R.string.preference_default_poi_city));
-
-		iatDialog.setParameter(SpeechConstant.SEARCH_AREA, province + city);
+		String province = "全选";
+		String city = "全选";
+		if (!TextUtils.isEmpty(app.getProvince())) {
+			province = app.getProvince();
+		}
+		if (!TextUtils.isEmpty(app.getCity())) {
+			city = app.getCity();
+		}
+		iatRecognizer.setParameter(SpeechConstant.SEARCH_AREA, province + city);
 
 		// 清空结果显示框.
 		tvPosition.setText(null);
@@ -180,98 +201,18 @@ public class VoiceSearchActivity extends BaseActivity implements OnClickListener
 		showShortToast(getString(R.string.text_iat_begin));
 	}
 
-	/**
-	 * 不显示听写对话框
-	 */
-	public void showIatInvisble() {
-		if (null == iatRecognizer) {
-			iatRecognizer = SpeechRecognizer.createRecognizer(this);
-		}
-		// 获取引擎参数
-		String engine = mSharedPreferences.getString(getString(R.string.preference_key_iat_engine),
-				getString(R.string.preference_default_iat_engine));
-
-		// 清空Grammar_ID，防止识别后进行听写时Grammar_ID的干扰
-		iatRecognizer.setParameter(SpeechConstant.CLOUD_GRAMMAR, null);
-		// 设置听写引擎
-		iatRecognizer.setParameter(SpeechConstant.DOMAIN, engine);
-		// 设置采样率参数，支持8K和16K
-		String rate = mSharedPreferences.getString(getString(R.string.preference_key_iat_rate),
-				getString(R.string.preference_default_iat_rate));
-		if (rate.equals("rate8k")) {
-			iatRecognizer.setParameter(SpeechConstant.SAMPLE_RATE, "8000");
-		} else {
-			iatRecognizer.setParameter(SpeechConstant.SAMPLE_RATE, "16000");
-		}
-
-		// 当DOMAIN设置为POI搜索时,获取area参数
-		String province = mSharedPreferences.getString(getString(R.string.preference_key_poi_province),
-				getString(R.string.preference_default_poi_province));
-		String city = mSharedPreferences.getString(getString(R.string.preference_key_poi_city),
-				getString(R.string.preference_default_poi_city));
-
-		iatRecognizer.setParameter(SpeechConstant.SEARCH_AREA, province + city);
-
-		// 清空结果显示框.
-		tvPosition.setText(null);
-		// 显示听写对话框
-		iatRecognizer.startListening(recognizerListener);
-		showShortToast(getString(R.string.text_iat_begin));
-	}
-
-	RecognizerListener recognizerListener = new RecognizerListener() {
-
-		@Override
-		public void onBeginOfSpeech() {
-			showShortToast("开始说话");
-		}
-
-		@Override
-		public void onError(SpeechError err) {
-			showShortToast(err.getPlainDescription(true));
-			((Button) findViewById(android.R.id.button1))
-					.setText(VoiceSearchActivity.this.getString(R.string.text_iat));
-			((Button) findViewById(android.R.id.button1)).setEnabled(true);
-		}
-
-		@Override
-		public void onEndOfSpeech() {
-			showShortToast("结束说话");
-		}
-
-		@Override
-		public void onEvent(int eventType, int arg1, int arg2, String msg) {
-
-		}
-
-		@Override
-		public void onResult(RecognizerResult results, boolean isLast) {
-			String text = JsonParser.parseIatResult(results.getResultString());
-			tvPosition.append(text);
-//			tvVoice.setSelection(tvVoice.length());
-			if (isLast) {
-				((Button) findViewById(android.R.id.button1)).setText(VoiceSearchActivity.this
-						.getString(R.string.text_iat));
-				((Button) findViewById(android.R.id.button1)).setEnabled(true);
-			}
-		}
-
-		@Override
-		public void onVolumeChanged(int volume) {
-			showShortToast("当前正在说话，音量大小：" + volume);
-		}
-
-	};
-
-	/**
+		/**
 	 * 识别回调监听器
 	 */
 	RecognizerDialogListener recognizerDialogListener = new RecognizerDialogListener() {
 		@Override
 		public void onResult(RecognizerResult results, boolean isLast) {
 			voiceStr = JsonParser.parseIatResult(results.getResultString());
-			tvPosition.append("“"+voiceStr+"”");
+			tvPosition.append(" " + voiceStr + " ");
 //			tvPosition.setSelection(tvPosition.length());
+			if(isLast){
+				synthetizeInSilence("您是要去"+tvPosition.getText().toString().replace("。", "") + "吗");
+			}
 		}
 
 		/**
@@ -302,5 +243,39 @@ public class VoiceSearchActivity extends BaseActivity implements OnClickListener
 		public void onEvent(int arg0, Bundle arg1) {
 		}
 	};
-
+	/**
+	 * 语音合成播放；
+	 */
+	private SynthesizerListener synthesizerListener = new SynthesizerListener() {
+		
+		@Override
+		public void onSpeakResumed() {
+			
+		}
+		
+		@Override
+		public void onSpeakProgress(int arg0, int arg1, int arg2) {
+			
+		}
+		
+		@Override
+		public void onSpeakPaused() {
+			
+		}
+		
+		@Override
+		public void onSpeakBegin() {
+			
+		}
+		
+		@Override
+		public void onCompleted(SpeechError arg0) {
+			
+		}
+		
+		@Override
+		public void onBufferProgress(int arg0, int arg1, int arg2, String arg3) {
+			
+		}
+	};
 }
