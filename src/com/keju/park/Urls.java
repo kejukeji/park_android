746ab0e8748de7519cc13633b440065d07287539
ZhoujunAndroid <zhoujun@zhoujundeMacBook-Air.local>
2014-05-16 16:13:43 +0800

    下拉刷新上拉更多

---
+++ src/com/keju/park/Urls.java
@@ -0,0 +1,12 @@
+package com.keju.park;
+
+/**
+ * url地址
+ * @author Zhoujun
+ * @version 创建时间：2014年5月16日 下午2:29:49
+ */
+public class Urls {
+	public static final String BASE_URL = "http://park.kejukeji.com";
+	public static final String URL_PARK_LIST = BASE_URL + "/ssbusy/carbarn/latitude-longitude?";
+}
+
