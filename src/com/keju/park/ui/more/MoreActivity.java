ddf2b3301d8f95d60f05b9c6a00c0a20933da58b
mygithuba <510301995@qq.com>
2014-05-17 16:02:57 +0800

    更多...版本更新

--- src/com/keju/park/ui/more/MoreActivity.java
+++ src/com/keju/park/ui/more/MoreActivity.java
@@ -8,6 +8,10 @@ import android.widget.RelativeLayout;
 import com.keju.park.Constants;
 import com.keju.park.R;
 import com.keju.park.ui.base.BaseActivity;
+import com.keju.park.util.NetUtil;
+import com.umeng.update.UmengUpdateAgent;
+import com.umeng.update.UmengUpdateListener;
+import com.umeng.update.UpdateResponse;
 
 /**
  * 更多界面
@@ -17,7 +21,8 @@ import com.keju.park.ui.base.BaseActivity;
  * @version 1.0
  */
 public class MoreActivity extends BaseActivity implements OnClickListener {
-	private RelativeLayout rlaboutWe, rlcontactWe, rlversionUpdate, rlIdea, rlShare;
+	private RelativeLayout rlaboutWe, rlcontactWe, rlversionUpdate, rlIdea,
+			rlShare;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -62,12 +67,41 @@ public class MoreActivity extends BaseActivity implements OnClickListener {
 			openActivity(ContactMeActivity.class);
 			break;
 		case R.id.rlversionUpdate:
+			if (NetUtil.checkNet(this)) {
+				UmengUpdateAgent.update(this);
+				UmengUpdateAgent.setUpdateOnlyWifi(false);
+				UmengUpdateAgent.setUpdateAutoPopup(false);
+				UmengUpdateAgent.setUpdateListener(new UmengUpdateListener() {
+					@Override
+					public void onUpdateReturned(int updateStatus,
+							UpdateResponse updateInfo) {
+						switch (updateStatus) {
+						case 0: // has update
+							UmengUpdateAgent.showUpdateDialog(
+									MoreActivity.this, updateInfo);
+							break;
+						case 1: // has no update
+							showShortToast("已经是最新版本");
+							break;
+						case 2: // none wifi
+							showShortToast("没有wifi连接， 只在wifi下更新");
+							break;
+						case 3: // time out
+							showShortToast("连接服务器超时");
+							break;
+						}
+					}
+				});
+			}else {
+				showShortToast(R.string.NoSignalException);
+			}
 			break;
 		case R.id.rlIdea:
 			openActivity(IdeaFeedBackActivity.class);
 			break;
 		case R.id.rlShare:
-			recommandToYourFriend(Constants.APP_DOWNLOAD_URL, "我发现一个很好的 找创业园区的APP，下载地址：");
+			recommandToYourFriend(Constants.APP_DOWNLOAD_URL,
+					"我发现一个很好的 找创业园区的APP，下载地址：");
 			break;
 		default:
 			break;
